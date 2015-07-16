32cd46f7eb11778eeb652ebb9455c8628b168b75
nightwing891 <henrydoan96@gmail.com>
2015-07-15 20:32:45 -0600

    First

--- net-watcher.js
+++ net-watcher.js
@@ -1,42 +1,33 @@
-"use strict";
+'use strict';
 const
+
     fs = require('fs'),
     net = require('net'),
+
     filename = process.argv[2],
-    server = net.createServer(function(connection){
-        /*
-        It reports that the connection has been established
-        (both to the client with connection.write, and to the console).
-        */
+
+    server = net.createServer(function(connection) {
+
+        // reporting
         console.log('Subscriber connected.');
         connection.write("Now watching '" + filename + "' for changes...\n");
-        /*
-        It begins listening for changes to the target file, saving the
-        returned watcher object. This callback sends change information
-        to the client using connection.write
-        */
-        let watcher = fs.watch(filename, function(){
-            connection.write("File '" + filename + "' changed: "  + Date.now() + "\n");
+
+        // watcher setup
+        let watcher = fs.watch(filename, function() {
+            connection.write("File '" + filename + "' changed: " + Date.now() + "\n");
         });
-        /*
-        It listens for the connection's close event so it can report that
-        the subscriber has diconnected and stop watching the file, with
-        watcher.close()
-        */
-        connection.on('close', function(){
+
+        // cleanup
+        connection.on('close', function() {
             console.log('Subscriber disconnected.');
             watcher.close();
         });
 
-        connection.on('error', function(err){
-            throw Error('There was an error\n', err);
-        })
     });
+if (!filename) {
+    throw Error('No target filename was specified.');
+}
 
-    if(!filename){
-        throw Error('No target filename was specified.');
-    }
-
-    server.listen(5432, function(){
-        console.log('Listening for subsrcibers...');
-    });
+server.listen(5432, function() {
+    console.log('Listening for subscribers...');
+});
